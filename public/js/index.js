b43832eee6d4d9bcbfb011991f28887a4294d068
unknown <1211071880@qq.com>
2017-04-27 23:37:16 +0800

    init this app completely first step

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Alan
+ * @Date:   2017-04-27 23:19:09
+ * @Last Modified by:   Alan
+ * @Last Modified time: 2017-04-27 23:28:52
+ */
+
+'use strict';
+
+(function main() {
+	console.log('test');
+})()
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Alan
+ * @Date:   2017-04-27 23:11:26
+ * @Last Modified by:   Alan
+ * @Last Modified time: 2017-04-27 23:18:13
+ */
+
+'use strict';
+
+module.exports = {
+	entry: __dirname + '/public/js/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: __dirname + '/dist'
+	}
+}
